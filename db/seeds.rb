657f763e9dccaa7b74830f2fbe62a652b9d4cdc8
Christina Barreti Sigal <CBarrettiSigal@gmail.com>
2020-02-19 16:49:51 -0500

    really ugly but done with the necessary actions in Yelp MVP

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,6 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+Restaurant.destroy_all
 #   Character.create(name: 'Luke', movie: movies.first)
+restaurants = Restaurant.create!([{name: 'Chez Claudette', address: "Laurier", category: "french"}, {name: "Areperia", address: "Sq. St. Louis", category: "belgian"}, {name: "Juliette e Chocolat", address: "Laurier O", category: "french"}, {name: "Yokoto Yokabai", address: "Drolet", category: "japanese"}, {name: "Kouign Amann", address: "Mont-Royal", category: "french"}])
